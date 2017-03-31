ad760b2ac1610c4ffb707e63d4cf42500b1bfa11
mattjared <mattjared9@gmail.com>
2017-03-30 21:10:14 -0500

    []

--- _harp/scripts/script.js
+++
@@ -1,9 +0,0 @@
-function footerize() {
-    var footerHeight = $('footer').outerHeight();
-    $('.spacer').height(footerHeight);
-    $('.body-contain').css({'marginBottom':'-' + footerHeight + 'px'});
-}
-footerize();
-$(window).resize(function() {
-    footerize();
-});
