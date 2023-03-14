745c4312b1b0763ea87cd4b586e68cad8e42ed56
Matt Jared <matt.jared@vercel.com>
2023-03-14 14:27:36 -0500

    committing

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 // import { Analytics } from '@vercel/analytics/react';
 import Footer from './Footer';
-import Header from './Navbar';
+import Navbar from './Navbar';
 import { Source_Code_Pro } from "next/font/google";
 const sourceCodePro = Source_Code_Pro({ subsets: ['latin'] })
 import './globals.css';
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`bg-mainBlue text-altBlue ${sourceCodePro.className}`}>
         <div className="py-32 px-8 max-w-4xl min-h-screen transition-all m-auto">
-          <Header />
+          <Navbar />
           { children }
           <Footer />
         </div>
