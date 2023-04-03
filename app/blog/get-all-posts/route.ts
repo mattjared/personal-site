00f8d998b0a0886b29ed93b09c4312cc6ef30329
Matt Jared <matt.jared@vercel.com>
2023-04-03 12:16:31 -0500

    Barry Parkhill

--- app/blog/get-all-posts/route.ts
+++ app/blog/get-all-posts/route.ts
@@ -2,6 +2,8 @@ import fs from "fs";
 import { join } from "path";
 import graymatter from "gray-matter";
 const postsDirectory = join(process.cwd(), "_posts");
+import { NextResponse } from 'next/server'
+
 
 export const revalidate = 0 // disable cache
 
@@ -25,5 +27,5 @@ export async function GET() {
     })
   });
   const options = { status: 200, revalidate }
-  return new Response(JSON.stringify(allPosts), options);
+  return new NextResponse(JSON.stringify(allPosts), options);
 }
