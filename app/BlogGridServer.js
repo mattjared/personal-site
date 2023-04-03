14358dc03b7914d1d4c4a8f1b5d87f684876b143
Matt Jared <matt.jared@vercel.com>
2023-04-03 12:01:12 -0500

    Lazaro Borrell

--- app/BlogGridServer.js
+++ app/BlogGridServer.js
@@ -20,10 +20,22 @@ export default async function BlogGridServer() {
   return (
     <div>
       {route}
+      <div className="grid gap-1 grid-cols-10">
+        {allBlogs.map((p, i) => {
+          return (
+            <div className="border" key={`${i}-${p}-top`}>
+              <p>{p.slug}</p>
+              <p>{p.title}</p>
+              <p>{p.postDate}</p>
+              <p>{p.published && "published"}</p>
+            </div>
+          )
+        })}
+      </div>
       <div className="mb-8 grid gap-8 grid-cols-1 md:grid-cols-3">
         {allBlogs.map((post, i) => {
           return (
-            <Box key={i}>
+            <Box key={`${i}-${post}-bottom`}>
               <Link href={`/blog/${post.slug}`}>
                 <h3>{post.title}</h3>
                 <p><small>{post.postDate}</small></p>
